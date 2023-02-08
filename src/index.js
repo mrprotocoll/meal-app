4212db0c3d033c3330174e9fedfbbf5b1c2a5077
Lancelot-SO <fsowah001@gmail.com>
2023-02-08 13:31:56 +0000

    added popup section

--- src/index.js
+++ src/index.js
@@ -8,11 +8,26 @@ window.addEventListener('load', () => {
   // meal container
   const mealsContainer = document.getElementById('meals');
 
-  const getAllMeals = () => {
-    meal.getMeals().then((meals) => {
+  const getAllMeals = async () => {
+    await meal.getMeals().then((meals) => {
       mealsContainer.innerHTML = Template.meals(meals);
     });
   };
 
-  getAllMeals();
+  getAllMeals().then(() => {
+    const popupcontainer = document.getElementById('pop-up');
+
+    const commentbtn = document.querySelectorAll('.comment-action');
+    Array.from(commentbtn).forEach((btn) => {
+      btn.onclick = async () => {
+        const iD = btn.getAttribute('data-id');
+        await meal.getMealByID(iD).then((meal) => {
+          popupcontainer.innerHTML = Template.comment(meal.meals[0]);
+        });
+        document.querySelector('.close-btn').onclick = () => {
+          popupcontainer.innerHTML = '';
+        };
+      };
+    });
+  });
 });
--- src/module/Template.js
+++ src/module/Template.js
@@ -9,7 +9,7 @@ export default class Template {
             <div class="likes"><span><i class="fa fa-heart"></i> 5 likes</span></div>
         </div>
         <div class="action">
-            <button>Comments</button>
+            <button class="comment-action" data-id="${meal.idMeal}">Comments</button>
         </div>
     </div>`
 
@@ -20,4 +20,53 @@ export default class Template {
       });
       return content;
     };
+
+    static getIngredients = (meal) => {
+      let content = '';
+      for (let x = 1; x <= 10; x += 1) {
+        content += `<span>${meal[`strIngredient${x}`]}</span>`;
+      }
+      return content;
+    }
+
+    static comment = (meal) => `<section class="com-section">
+        <div class="comment-section">
+          <div class="image">
+            <img
+              src="${meal.strMealThumb}"
+              alt="${meal.strMeal}"
+            />
+            <div class="close-btn">
+                <i class="fa fa-times" aria-hidden="true"></i>
+            </div>
+          </div>
+          <div class="food-name">
+            <h2>${meal.strMeal}</h2>
+          </div>
+          <div class="food-recipe">
+            <h3>Ingredients</h3>
+            <div class="ingredients">
+             ${this.getIngredients(meal)}
+            </div>
+          </div>
+          <div class="description">
+            <p>${meal.strInstructions}</p>
+          </div>
+          <div class="comments">
+            <h3>Comments<span>5</span></h3>
+            <ul>
+              <li>Felix</li>
+              <li>Felix lancelot</li>
+              <li>Felix</li>
+            </ul>
+          </div>
+
+          <form class="form" id="form">
+            <div class="add-comment">Add a comment</div>
+            <input type="text" placeholder="Your name " />
+            <textarea placeholder="Your insights" rows="5" cols="20"></textarea>
+            <button type="submit" class="btn">Comments</button>
+          </form>
+        </div>
+      </section>`;
 }
