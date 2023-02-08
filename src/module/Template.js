38e6b7321dfe855c956112714b19729fb4e2d2a7
mrprotocoll <lekanvgbg@gmail.com>
2023-02-08 09:41:29 +0100

    Added script to fetch meals from api

---
+++ src/module/Template.js
@@ -0,0 +1,24 @@
+export default class Template {
+    static meal = (meal) => `
+    <div class="meal" data-id="${meal.idMeal}">
+        <div class="meal-image">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        </div>
+        <div class="title">
+            <h2>${meal.strMeal}</h2>
+            <div class="likes"><span><i class="fa fa-heart"></i> 5 likes</span></div>
+        </div>
+        <div class="action">
+            <button>Comments</button>
+        </div>
+    </div>`
+
+    static meals = (data) => {
+        let content = '';
+        data.meals.map((meal) => {
+            content += this.meal(meal);
+        });
+        return content;
+    };
+    
+}
