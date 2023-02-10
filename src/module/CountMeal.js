0e6fd4945c08c2cdeec60fb021d2df99b82ad81b
mrprotocoll <lekanvgbg@gmail.com>
2023-02-10 07:15:43 +0100

    refactored index.js and fixed Linters error

--- src/module/CountMeal.js
+++ src/module/CountMeal.js
@@ -1,7 +1,7 @@
 const CountMeal = () => {
-    const numberOfMeals  = document.querySelector('#meals').childElementCount
-    const nav = document.querySelector('.nav-links .active').firstElementChild
-    nav.textContent = `(${numberOfMeals})`
-}
+  const numberOfMeals = document.querySelector('#meals').childElementCount;
+  const nav = document.querySelector('.nav-links .active').firstElementChild;
+  nav.textContent = `(${numberOfMeals})`;
+};
 
-export default CountMeal
+export default CountMeal;
